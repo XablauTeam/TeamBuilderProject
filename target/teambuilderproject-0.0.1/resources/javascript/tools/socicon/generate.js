fb074f98634762859803e1ea725c317a17aad42b
Paulo Lopes <paulohq@info.urn.br>
2017-06-06 07:49:53 -0300

    Deleting target

--- target/teambuilderproject-0.0.1/resources/javascript/tools/socicon/generate.js
+++
@@ -1,7 +0,0 @@
-
-    var icons = require('./icons.json');
-
-    for (var i = 0; i < icons.length; i++) {
-        var icon = icons[i];
-        console.log("." + icon.id + " { color: " + icon.color + "; }");
-    };
